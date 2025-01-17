1654fe3346dfb2a717c7ccadaca8d1215185b9f5
Pelpav <elpav2022@gmail.com>
2025-01-17 18:41:36 +0000

    Models finalisation

--- myauth/models.py
+++ myauth/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-class User(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+# class User(models.Model):
+#     firstname = models.CharField(max_length=255)
+#     lastname = models.CharField(max_length=255)
+#     email = models.EmailField(unique=True)
+#     username = models.CharField(max_length=255, unique=True)
+#     password = models.CharField(max_length=255)

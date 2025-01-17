e1bdb4776ebf66c42dc5b1adcf5212beff4920ee
Pelpav <elpav2022@gmail.com>
2025-01-17 17:32:59 +0000

    Création des apps de base

--- christian_nouboukpo_et_salim_bittar/settings.py
+++ christian_nouboukpo_et_salim_bittar/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'christian_nouboukpo_et_salim_bittar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'parking.sqlite3',
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
